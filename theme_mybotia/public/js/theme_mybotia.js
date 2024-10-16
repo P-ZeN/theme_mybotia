4c416bf510de87379a8338a997c77868c824b287
P-ZeN <philippe.zenone@free>
2024-10-16 21:59:17 +0200

    test2

--- theme_mybotia/public/js/theme_mybotia.js
+++ theme_mybotia/public/js/theme_mybotia.js
@@ -1,1 +1,36 @@
 console.log("Hu!!!");
+
+frappe.ui.ThemeSwitcher = class CustomThemeSwitcher extends frappe.ui.ThemeSwitcher {
+    constructor() {
+        super();
+    }
+
+    fetch_themes() {
+        return new Promise((resolve) => {
+            this.themes = [
+                {
+                    name: "light",
+                    label: "Frappe Light",
+                    info: "Light Theme",
+                },
+                {
+                    name: "dark",
+                    label: "Timeless Night",
+                    info: "Dark Theme",
+                },
+                {
+                    name: "theme_mybotia",
+                    label: "MyBot IA Custom thème",
+                    info: "A modern theme for MyBot IA",
+                },
+                {
+                    name: "automatic",
+                    label: "Automatic",
+                    info: "Uses system's theme to switch between light and dark mode",
+                },
+            ];
+
+            resolve(this.themes);
+        });
+    }
+};
